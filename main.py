f7b8e86bca4bf67523734ec1f9c01f5871b826f3
Sergei Shchepa <d.ironclad@gmail.com>
2023-12-05 08:47:14 +0100

    Day 4.2 unfinished. Failing to see my mistake :')

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
     # print(f"Day 1:\n  {calibration()}")
     # print(f"Day 2.1:\n {cubes_game()}")
     # print(f"Day 2.2:\n {cubes_game(do_break=False)}")
-    print(f"Day 3: {engine()}")
+    # print(f"Day 3: {engine()}")
     print(f"Day 4: {cards()}")
--- tasks/day4.py
+++ tasks/day4.py
@@ -1,23 +1,43 @@
+import pprint
 import re
-from itertools import accumulate
 from .utils import get_file
 
 NUMBERS_PATTERN = '\d+'
+
+def get_score(card: dict):
+    intersection = list(set(card["have"]) & set(card["win"]))
+    score = 0
+    wins = len(intersection)
+    if len(intersection) > 0:
+        score =  [1 * 2**i for i in range(len(intersection))][-1]
+    return score, wins
+
 def cards():
     lines = get_file("data_day4.txt")
     cards = []
+    wins = []
     score = 0
     for line in lines:
         numbers = line.split(":")[1].split("|")
-        cards.append(
-            {"num": line.split(":")[0], "win": re.findall(NUMBERS_PATTERN, numbers[0]), "have": re.findall(NUMBERS_PATTERN, numbers[1])}
-        )
+        score, win = get_score({"win": re.findall(NUMBERS_PATTERN, numbers[0]), "have": re.findall(NUMBERS_PATTERN, numbers[1])})
+        cards.append(score)
+        wins.append(win)
 
-    print(cards)
-    for card in cards:
-        intersection = list(set(card["have"]) & set(card["win"]))
-        if len(intersection) > 0:
-            progression = [1 * 2**i for i in range(len(intersection))]
-            score += progression[-1]
-
-    return score
+    pprint.pprint(cards, depth=3)
+    score = sum(cards)
+    counter = 0
+    # go through cards
+    print(wins)
+    for idx, card in enumerate(wins):
+        print(f"Index {idx+1} and wins", card)
+        # determine amount and indexes of copies?
+        # card itself and how many it wins
+        if card  > 0:
+            counter += card + 1 
+            # win of each copy
+            for i in range(idx+1, idx+card+1):
+                print(f"Won {wins[i]} from card", i+1)
+                if wins[i]:
+                    counter += wins[i]+1
+                print("Get cards ", counter)
+    return score, counter
